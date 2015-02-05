3b9ca5bb28f6a470eb92e158e211f5de2a453df0
Matthew Ahlberg <mjahlber@gmail.com>
2015-02-05 10:43:00 -0800

    added ng-repeat and reset button function

--- app.js
+++ app.js
@@ -3,5 +3,50 @@ angular.module('scheduleApp', ['firebase'])
 
 // create our main controller and get access to firebase
 .controller('mainController', function($scope, $firebase) {
-	// our application code will go here
-})
+  
+  // connect to firebase 
+  var ref = new Firebase("https://burning-torch-4263.firebaseio.com/days");  
+	// notice days added to the end of url, it will map out our data and will all be saved in the 'days' object in our database
+  var fb = $firebase(ref);
+
+  // sync as object
+  var syncObject = fb.$asObject();
+
+  // three way data binding
+  syncObject.$bindTo($scope, 'days');
+
+  // function to set the default data
+  $scope.reset = function() {    
+
+    fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      }
+    });    
+
+  };
+  
+});
